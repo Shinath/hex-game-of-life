cb5505b3a725e535b6e887167295af3d11e2319e
Wiktoria Kalata <wkalata4s@gmail.com>
2023-11-08 12:35:18 +0100

    init

---
+++ src/main/java/com/wit/hex/game/of/life/HexGameOfLife.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.wit.hex.game.of.life;
+
+/**
+ *
+ * @author wiks
+ */
+public class HexGameOfLife {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
